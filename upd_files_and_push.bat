c18cdc773999f9785236b13910fcc2661afc10a3
Christian Bauermeister <Christian@dkpunktum.dk>
2019-05-09 09:30:39 +0200

    auto-updates

--- upd_files_and_push.bat
+++ upd_files_and_push.bat
@@ -1,4 +1,4 @@
-xcopy C:\Users\Christian\PycharmProjects\database_setup /S /E /F /EXCLUDE:exclude_from_copy.txt
+xcopy C:\Users\Christian\PycharmProjects\database_setup /Y /S /E /F /EXCLUDE:exclude_from_copy.txt
 git add .
 git commit -m "auto-updates"
 git push -u origin master
